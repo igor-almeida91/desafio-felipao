43a894dee613c229cf463d42642fbef34219498e
igor-almeida91 <igor.91.almeida2@gmail.com>
2025-03-26 21:45:24 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-let msg1 = "Hello"
-let msg2 = "Wolrd"
+// # 1 Desafio Classificador de nivel de Herói
+let nomeDoHeroi = "Wyclif"
+let nivel = 7001
+let rank = ""
 
-console.log (msg1 + ", " + msg2)
+if (nivel <= 1000){
+rank = "Ferro"
+}
+else if(nivel >=1001 && nivel<=2000){
+    rank = "Bronze"
+}
+else if(nivel >=2001 && nivel<=5000){
+    rank = "Prata"
+}
+else if(nivel >=5001 && nivel<=7000){
+    rank = "Ouro"
+}
+else if(nivel >=7001 && nivel<=8000){
+    rank = "Platina"
+}
+else if(nivel >=8001 && nivel<=9000){
+    rank = "Ascendente"
+}
+else if(nivel >=9001 && nivel<=10000){
+    rank = "Imortal"
+}
+else if (nivel >=10001){
+    rank = "Radiante"
+}
+
+console.log("O Herói de nome " + nomeDoHeroi + " está no nivel de " + rank)
+
+
